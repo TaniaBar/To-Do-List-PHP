1056adf6a035e083574c2ba9060de87f3a6e2e1a
TaniaBar <148057407+TaniaBar@users.noreply.github.com>
2024-02-15 16:06:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -40,9 +40,6 @@ if(isset($_GET['doneTask']) && isset($_GET['stat'])) {
 
     header('location: index.php');
 }
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -127,36 +124,12 @@ if(isset($_GET['doneTask']) && isset($_GET['stat'])) {
                 </div>
             </div>
         </div>
-    </section>
-        
+    </section>    
 
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script>
-        // const finishButtons = document.querySelectorAll('.finishTask');
-
-        // finishButtons.forEach(button => {
-        //     button.addEventListener('click', function() {
-                // const parentRow = this.closest('tr');
-        //         const nameTask = parentRow.querySelector('#nameTask');
-        //         nameTask.style.textDecoration = "line-through";
-        //         this.style.opacity = "0.5";
-        //         this.disable = "true";
-        //         // saveTask();   
-        //     })
-        // })
-
-        // function saveTask() {
-        //     localStorage.setItem('task', listContainer.innerHTML);
-        // }
-
-        // function showTask() {
-        //     listContainer.innerHTML = localStorage.getItem('task');
-        // }
-        // showTask();
-
-    </script>
+    
 </body>
 </html>
